b596f27f1a989b1d4a9d394db401369c1a23c464
unknown <sarasaravevic@gmail.com>
2022-05-11 00:43:17 +0200

    commit

--- osnovni tipovi podataka/app.js
+++ osnovni tipovi podataka/app.js
@@ -15,8 +15,12 @@
 // %
 // https://www.w3schools.com/js/js_arithmetic.asp
 
-ime = "sara";
-console.log(ime);
+// ime = "sara";
+// console.log(ime);
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+// console.log("s" === "s");
 
 // console.log(true && true)
 // console.log(true && false)
@@ -32,3 +36,22 @@ console.log(ime);
 
 //https://www.youtube.com/watch?v=FSs_JYwnAdI&list=PL0vfts4VzfNixzfaQWwDUg3W5TRbE7CyI&index=2
 //https://www.youtube.com/watch?v=DHjqpvDnNGE
+
+// broj = Number(prompt("Unesite neki broj"));
+// console.log(broj);
+
+// broj = String(prompt("Unesite neki broj"));
+// console.log(broj);
+// x = Number(prompt("unesite neki broj"));
+// if (x % 2 === 0) {
+//   console.log("broj je deljiv sa 2");
+// } else {
+//   console.log("broj  nije deljiv sa 2 ");
+// }
+// x = String(prompt("unesite neku rec"));
+// if (x === x + x) {
+//   console.log("string je dupliran");
+// }
+// if (x === x) {
+//   console.log("string nije dupliran");
+// }
