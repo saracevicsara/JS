b28de118a7291787c94c685b9af517be87d483a8
unknown <sarasaravevic@gmail.com>
2022-05-10 23:19:45 +0200

    js

---
+++ osnovni tipovi podataka/app.js
@@ -0,0 +1,34 @@
+//Primitive data types:
+
+//string:
+// "a234821390sadjlsakd"
+//number:
+// 25
+//boolean:
+// true , false
+
+// +
+// -
+// *
+// /
+// **
+// %
+// https://www.w3schools.com/js/js_arithmetic.asp
+
+ime = "sara";
+console.log(ime);
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false&&true)
+// console.log(false && false)
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+//https://www.w3schools.com/js/js_operators.asp
+
+//https://www.youtube.com/watch?v=FSs_JYwnAdI&list=PL0vfts4VzfNixzfaQWwDUg3W5TRbE7CyI&index=2
+//https://www.youtube.com/watch?v=DHjqpvDnNGE
