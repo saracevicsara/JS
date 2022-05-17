9c355b573f79bbba4cd4c7c7254d3e884d7741c1
unknown <sarasaravevic@gmail.com>
2022-05-17 21:12:15 +0200

    commit

---
+++ vezbanje/app.js
@@ -0,0 +1,19 @@
+// array = [34, 56, 59];
+// array[1] = 67;
+// console.log(array);
+// names = ["sara", "ajsa", "aisa", "hana"];
+// for (i = 0; i <= names.lenght; i++) {
+//   console.log(names[i]);
+// }
+// i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+//   if (i === 6) break;
+// }
+i = 0;
+do {
+  i++;
+  if (i === 5) continue;
+  console.log(i);
+} while (i < 10);
