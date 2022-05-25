d21905b1966aaa9cef3ba700d9eb6631cd0eec8b
unknown <sarasaravevic@gmail.com>
2022-05-25 20:30:21 +0200

    commit

--- osnovni tipovi podataka/app.js
+++ osnovni tipovi podataka/app.js
@@ -362,17 +362,31 @@
 //   sum += num[i];
 // }
 // console.log(sum);
+// numbers = [3, 5, 10, 21, 42, 22, 1];
+// x = Number(prompt("unesite neki broj"));
+// founded = false;
+// for (i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === x) {
+//     founded = true;
+//     break;
+//   }
+// }
+// if (founded) {
+//   console.log("uneti broj postoji");
+// } else {
+//   console.log("uneti broj ne pripada nizu");
+// }
 numbers = [3, 5, 10, 21, 42, 22, 1];
-x = Number(prompt("unesite neki broj"));
-founded = false;
+najveci = numbers[0];
+najmanji = numbers[0];
 for (i = 0; i < numbers.length; i++) {
-  if (numbers[i] === x) {
-    founded = true;
-    break;
+  if (numbers[i] > najveci) {
+    najveci = numbers[i];
+  }
+
+  if (numbers[i] < najmanji) {
+    najmanji = numbers[i];
   }
 }
-if (founded) {
-  console.log("uneti broj postoji");
-} else {
-  console.log("uneti broj ne pripada nizu");
-}
+console.log(najveci);
+console.log(najmanji);
