8148bcebec44cfe5ef4594c029edb9e06ff3edeb
unknown <sarasaravevic@gmail.com>
2022-05-27 00:20:33 +0200

    commit

--- vezbanje/app.js
+++ vezbanje/app.js
@@ -44,3 +44,39 @@
 // br1 = 5;
 // br2 = 9;
 // console.log(add(br1, br2));
+// numbers = [2, 3, 5, 7, 9, 0, 76];
+// sum = 0;
+// for (i = 0; i < numbers.length; i++) {
+//   sum =sum+ numbers[i];//uzima vrednost sume i dodaje posebno svaki karakter iz niza
+// }
+// console.log(sum);
+// numbers = [2, 3, 5, 7, 9, 0, 76];
+// x = Number(prompt("probaj broj"));
+// founded = false;
+// for (i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === x) {
+//     founded = true;
+//     break;
+//   }
+// }
+// if (founded) {
+//   console.log("broj se sadrzi u nizu");
+// } else {
+//   console.log("broj se ne sadrzi u nizu");
+// }numbers = [2, 3, 5, 7, 9, 0, 76];
+// x = Number(prompt("probaj broj 2,3,5,7,9,0,76"));
+// index = 0;
+
+// for (i = 0; i < numbers.length; i++) {
+//   i = 0;
+//   while (i < numbers[i]) {
+//     if (numbers[i] === index) {
+//       i += 1;
+//     }
+//   }
+// }
+// console.log(index);
+//
+numbers = [2, 3, 5, 7, 9, 0, 76];
+numbers.splice(0, 1, 1001);
+console.log(numbers);
