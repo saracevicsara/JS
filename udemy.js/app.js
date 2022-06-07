f0ab0c8a14899ebbbae972c5a0cdb5b3b781e91e
unknown <sarasaravevic@gmail.com>
2022-06-07 22:34:59 +0200

    commit

---
+++ udemy.js/app.js
@@ -0,0 +1,19 @@
+// let userName = "Max";
+// function greetUser(name) {
+//   userName = name;
+//   console.log(userName);
+// }
+// userName = "Manu";
+// greetUser("Max");
+// function prva() {
+//   console.log("tandarabandara");
+// }
+// prva();
+// function ime(name) {
+//   console.log(name);
+// }
+// ime("sara");
+// function combined(a, b, c) {
+//   console.log(a + b + c);
+// }
+// combined("sara", "sara", "sara");
