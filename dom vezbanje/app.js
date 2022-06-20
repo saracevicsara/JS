2a4f8f80d29d4b7e8694439c792e9e6f41006351
unknown <sarasaravevic@gmail.com>
2022-06-20 20:55:24 +0200

    commit

---
+++ dom vezbanje/app.js
@@ -0,0 +1,15 @@
+// a = document.getElementById("paragraf");
+// function mojaf() {
+//   a.style.color = "red";
+//   a.style.fontSize = "50px";
+//   a.style.fontWeight = "bold";
+// }
+//------------------------------
+a = document.getElementById("paragraf");
+function mojaf() {
+  if (a.style.color === "red") {
+    a.style.color = "blue";
+  } else {
+    a.style.color = "red";
+  }
+}
---
+++ dom/app.js
@@ -0,0 +1,11 @@
+// a = document.getElementById("sara");
+// b = prompt("unesite svoje ime");
+// a.innerText = b;
+// div = document.getElementById("sara");
+// str = prompt("unesite svoje ime");
+// broj = Number(prompt("unesite koliko puta da se ponovi"));
+// let text = "";
+// for (i = 0; i < broj; i++) {
+//   text += str;
+// }
+// div.innerText = text;
