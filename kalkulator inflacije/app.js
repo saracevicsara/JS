c235d2ea2d2c7173b785a90cbb8ee3c89b3bd075
unknown <sarasaravevic@gmail.com>
2022-06-22 18:00:55 +0200

    kalkulator inflacije

---
+++ kalkulator inflacije/app.js
@@ -0,0 +1,15 @@
+function inflationCalculator() {
+  let inflationRate = document.querySelector("#inflationRate");
+  let money = document.querySelector("#money");
+  let years = document.querySelector("#years");
+  money = parseFloat(money.value);
+  inflationRate = parseFloat(inflationRate.value);
+  years = parseFloat(years.value);
+  let worth = money + money * (inflationRate / 100);
+  for (let i = 0; i < years; i++) {
+    worth += worth * (inflationRate / 100);
+  }
+  let newElement = document.createElement("div");
+  newElement.innerText = `Your worth after ${years} years is ${worth}`;
+  document.querySelector(".container").appendChild(newElement);
+}
