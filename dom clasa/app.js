ceeab464fc827f27cd0ab7402a139f7ddb00bb20
unknown <sarasaravevic@gmail.com>
2022-06-22 20:57:12 +0200

    commit

---
+++ dom clasa/app.js
@@ -0,0 +1,8 @@
+// text = document.querySelector("#text");
+
+// function addclass() {
+//   console.log("jfieh");
+//   if (text.classList.contains("red")) {
+//     text.classList.remove("red");
+//   } else text.classList.add("red");
+// }
--- kalkulator/app.js
+++ kalkulator/app.js
@@ -1,4 +1,9 @@
-function minuspet() {
-  let minusspet = document.querySelector("#minuspet");
-  minusspet.innerText = "-5";
+counterElement = document.querySelector("counter");
+function counterUp(value) {
+  let counter = Number(counterElement.innerText);
+  counterElement.innerText = counter + value;
+}
+function counterDown(value) {
+  let counter = Number(counterElement.innerText);
+  counterElement.innerText = counter - value;
 }
