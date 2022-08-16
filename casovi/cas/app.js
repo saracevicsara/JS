19ad7f1cd0a4981acf5aa30e1045445005c8db4a
unknown <sarasaravevic@gmail.com>
2022-08-16 18:40:58 +0200

    ...

--- casovi/cas/app.js
+++ casovi/cas/app.js
@@ -1,4 +1,4 @@
-// x = Number(prompt("unesite pocetnu vrednost"));
+// x = Number(prompt("unesite pocetnuu vrednost"));
 // y = Number(prompt("unesite krajnju vrednost"));
 // for (i = x; i <= y; i++) {
 //   console.log(i ** 2);
